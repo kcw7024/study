6187a4218b87822cb38a7f091da3f7cac0375766
kcw7024 <107664300+kcw7024@users.noreply.github.com>
2022-07-10 18:46:47 +0900

    commit

--- keras/keras27_cnn2_padding.py
+++ keras/keras27_cnn2_padding.py
@@ -1,5 +1,5 @@
 from tensorflow.python.keras.models import Sequential
-from tensorflow.python.keras.layers import Dense, Conv2D, Flatten #이미지작업(Conv2D), 데이터를 펼친다(Flatten)
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D #이미지작업(Conv2D), 데이터를 펼친다(Flatten)
 
 model = Sequential()
 
@@ -8,16 +8,20 @@ model = Sequential()
 
 # (input_dim + bias) * units = summary Param 갯수 (Dense모델)
 
-model.add(Conv2D(filters = 10, kernel_size=(3, 3), input_shape = (8, 8, 1))) # (batch_size, rows, colums, channels) # 출력 (None, 6, 6, 10)
+model.add(Conv2D(filters = 10, kernel_size=(3, 3), padding='same', input_shape = (28, 28, 1))) # (batch_size, rows, colums, channels) # 출력 (4, 4, 10)
 # 예제기준(Conv2D)
 # Conv2D 에서의 노드수(10)
 # kernerl_size = 이미지 크롭 규격
 # input_shape = 5x5, 흑백 이미지
 # 4+D tensor with shape: batch_shape + (channels, rows, cols) if data_format='channels_first' 
 # 4+D tensor with shape: batch_shape + (rows, cols, channels) if data_format='channels_last'
-# filters = output node의 수 
-model.add(Conv2D(7, (2, 2), activation='relu')) # 출력 (None, 5, 5, 7)
-model.add(Flatten()) # (None, 175)   
+# filters = output node의 수
+model.add(MaxPooling2D()) 
+model.add(Conv2D(32, (2, 2), 
+                    padding='valid', #디폴트 값(연산 후 결과값표기) (None, 7, 7, 7)
+                    #padding = 'same', #인풋값을 그대로 받는다(연산없이) (None, 8, 8, 7) 
+                    activation='relu')) # 출력 
+model.add(Flatten)
 model.add(Dense(32, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(10, activation='softmax'))
---
+++ keras/keras28_mnist2_cnn.py
@@ -0,0 +1,83 @@
+# 실습!!!!
+# acc 0.98 이상으로 만들어낼 것.
+# conv 3개 이상 쌓기
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D
+from tensorflow.keras.datasets import mnist
+import numpy as np
+from sklearn import datasets
+from tensorflow.keras.utils import to_categorical
+
+
+
+#1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+#print(x_train.shape, y_train.shape)   #(60000, 28, 28) (60000,)
+#print(x_test.shape, y_test.shape)     #(10000, 28, 28) (10000,)
+
+#원한인코딩 해준다.
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+#print(x_train.shape)
+#print(np.unique(y_train, return_counts=True))
+
+#(array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], dtype=uint8), array([5923, 6742, 5958, 6131, 5842, 5421, 5918, 6265, 5851, 5949],      
+
+
+
+
+
+#2. 모델 구성
+
+model = Sequential()
+# model.add(Dense(units=10, input_shape=(3,)))   #(batch_size, input_dim)
+# model.summary()
+# (input_dim + bias) * units = summary Params 갯수(Dense 모델)
+
+  
+model.add(Conv2D(filters = 64, kernel_size=(3,3),   # 출력 (N, 28, 28, 64) 패딩이 세임이므로
+                 padding='same',
+                 input_shape=(28, 28, 1))) # (batch_size, rows, cols, channels)  
+model.add(MaxPooling2D())
+model.add(Conv2D(32, (2,2), 
+                 padding='valid',  
+                 activation='relu'))  
+model.add(Conv2D(32, (2,2), 
+                 padding='valid', activation='relu'))
+model.add(Flatten())  # 출력 (N, 63)
+model.add(Dense(32, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+
+
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])  
+
+from tensorflow.python.keras.callbacks import EarlyStopping
+esp = EarlyStopping(monitor='val_loss', patience=10, mode='min', verbose=1,
+                restore_best_weights=True)
+
+hist = model.fit(x_train, y_train, epochs=1000, batch_size=128, validation_split=0.2, 
+                 callbacks=[esp], verbose=1) 
+
+
+
+
+#4. 평가, 예측
+loss, acc = model.evaluate(x_test, y_test)
+print('loss : ', loss)
+print('accuracy : ', acc)
+
+
+
+# loss :  0.06842680275440216
+# accuracy :  0.9805999994277954
+
+

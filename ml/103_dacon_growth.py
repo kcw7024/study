432842c3264e8a47373efcd3cd1ad1b6eb29f47c
kcw7024 <107664300+kcw7024@users.noreply.github.com>
2022-08-31 18:29:43 +0900

    commit

--- ml/103_dacon_growth.py
+++ ml/103_dacon_growth.py
@@ -45,7 +45,7 @@ device = torch.device(
     'cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 CFG = {
-    'EPOCHS': 5,
+    'EPOCHS': 50,
     'LEARNING_RATE': 1e-3,
     'BATCH_SIZE': 16,
     'SEED': 41
